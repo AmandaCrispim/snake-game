624f8ec9640c4a1a3771d6aa9f3700c5c6fb74cf
Amanda  Crispim <90980799+AmandaCrispim@users.noreply.github.com>
2024-02-29 21:16:33 +0000

    bug-fix: finalScore e initialPosition

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 
 const score = document.querySelector('.score-value')
-const finalScore = document.querySelector('.final-value > span')
+const finalScore = document.querySelector('.final-score > span')
 const menu = document.querySelector('.menu-screen')
 const buttonPlay = document.querySelector('.btn-play')
 
